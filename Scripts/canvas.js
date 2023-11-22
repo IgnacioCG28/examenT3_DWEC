d0121b72487d3e05fa9a79989836d49ec39988d5
IgnacioCG28 <ignaciocarmonagonzalez@alumnos.tuniversformacion.es>
2023-11-22 09:02:59 +0100

    Correcion error 3: largo del canvas width

--- Scripts/canvas.js
+++ Scripts/canvas.js
@@ -1,7 +1,8 @@
 const canvas = document.querySelector('canvas');
 export const ctx = canvas.getContext('2d');
 
-export const width = canvas.width = window.innerHeight;
+//Error de innerHeight a innerWidth
+export const width = canvas.width = window.innerWidth;
 export const height = canvas.height = window.innerHeight;
 
 export function random(min, max) {
